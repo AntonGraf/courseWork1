9b2e9762cfa82bd61c0267ad45de4b39b7420bfb
AntonGraf <ant0ngraf@yandex.ru>
2022-09-07 15:40:46 +0600

    Добавляет метод поиска первого свободного элемента и Exception, когда массив полный

--- src/pro/sky/java/course1/courseWork/EmployeeBook.java
+++ src/pro/sky/java/course1/courseWork/EmployeeBook.java
@@ -7,5 +7,18 @@ public class EmployeeBook {
     //Счетчик
     private int counter = 0;
 
-    
+    /*
+    Находит индекс первой пустой ячейки.
+     */
+    private static int getFirstFreeElement(Employee[] employees) throws ArrayIsFullExceptions {
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                return i;
+            }
+        }
+
+        throw new ArrayIsFullExceptions("Нет свободных ячеек в массиве");
+
+    }
 }
