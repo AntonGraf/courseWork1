319b89c8f634f1dea92fa46d7095aa9293a81b71
AntonGraf <ant0ngraf@yandex.ru>
2022-09-07 14:41:32 +0600

    Добавляет класс EmployeeBook - сервис по работе со списком сотрудников

---
+++ src/pro/sky/java/course1/courseWork/EmployeeBook.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course1.courseWork;
+
+public class EmployeeBook {
+
+    //Список сотрудников
+    private Employee[] employees;
+    //Счетчик
+    private int counter = 0;
+
+    
+}
